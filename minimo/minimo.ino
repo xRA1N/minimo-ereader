c2bb1ba1681ea3d1f1f13cae2559843537dfbce5
xRA1N <15974550+xRA1N@users.noreply.github.com>
2019-06-20 18:58:37 +0800

    Update minimo.ino
    修正汉字错位问题

--- minimo/minimo.ino
+++ minimo/minimo.ino
@@ -9,28 +9,42 @@
 #include <U8g2lib.h>
 U8G2_ST7565_ERC12864_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ D2, /* dc=*/ D1, /* reset=*/ D9);
 
-int i ;
-int y ;
-int x ;
-char l;
-int fontstep ;
+int i ;       // 读取文件索引
+int y ;       // 屏幕光标位置
+int x ;       // 屏幕光标位置
+char l;      //文件当前字符
+int ihan ; //汉字字节计数
 
+
+void showlogo() {
+  u8g2.clearBuffer();
+  u8g2.setCursor(10, 35);
+  u8g2.setFont( u8g2_font_bubble_tr);   //中文字体
+  u8g2.print( "MINIMO" );
+  u8g2.sendBuffer();
+  delay(1000);
+  SPIFFS.end();
+}
 void CheckFS() {                //检查文件系统
   bool fsc = SPIFFS.begin();
   if (!fsc) {
     u8g2.clearBuffer();
-    u8g2.setCursor(0, 20);
-    u8g2.print( "SPIFFS文件系统故障" );
+    u8g2.setCursor(0, 15);
+    u8g2.setFont(u8g2_font_wqy12_t_gb2312);   //中文字体
+    u8g2.print( "   SPIFFS文件系统故障" );
     u8g2.sendBuffer();
+    SPIFFS.end();
     ESP.deepSleep(30e6);
   }
 }
 
 void CheckFile() {          //检查文件
+  SPIFFS.begin();
   bool file_exist = SPIFFS.exists("/txt");
   if (file_exist) {
     u8g2.clearBuffer();
     u8g2.setCursor(0, 20);
+    u8g2.setFont(u8g2_font_wqy12_t_gb2312);   //中文字体
     u8g2.print( "找到txt文件" );
     u8g2.sendBuffer();
     delay(2000);
@@ -64,49 +78,67 @@ void CheckFile() {          //检查文件
     delay(1300);
   }
   f.close();
+  SPIFFS.end();
 }
+
+void plot_asc() {
+  if (l == 10) {
+    i = i + 1;
+    x = 0;
+    y = y + 12;
+    u8g2.setCursor(0, y);
+  }
+  else {
+    u8g2.setFont( u8g2_font_unifont_t_latin  );  // choose a suitable font
+    u8g2.print(l);
+    i = i + 1;
+    x = x + 7;
+  }
+
+}
+
+void plot_han() {
+  u8g2.setFont(u8g2_font_wqy12_t_gb2312);   //中文字体
+  u8g2.print(l);
+  i = i + 1;
+  ihan = ihan + 1;
+  if (ihan > 2) {
+    ihan = 0;
+    x = x + 12;
+  }
+}
+
 
 void setup() {
   u8g2.begin();
   u8g2.enableUTF8Print();                                       //utf8支持
   u8g2.setFont(u8g2_font_wqy12_t_gb2312);   //中文字体
-  //u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
+  u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
   Serial.begin(9600);
-  SPIFFS.begin();
+  //  SPIFFS.begin();
+  showlogo();
   CheckFS();            //检查文件系统
   CheckFile();        //检查文件
   u8g2.clearBuffer();
+
 }
 
 void loop() {
-  File f = SPIFFS.open("/txt", "r");
+  SPIFFS.begin();
   //  Serial.print("i =");
   //  Serial.println(i);
+  File f = SPIFFS.open("/txt", "r");
   f.seek(i, SeekSet);
   l = f.read();
-  i = i + 1;
-  Serial.print("l =");
-  Serial.println(int(l));
 
-  if (l == 10) {   //检测回车l的 ascii == 10
-    x = 0 ;
-    y = y + 12;
-  }
-  if (l > 224) {//检测到 1110 0000,3字节UTF-8字符，
-    fontstep = 4;
-  }
-  if (l < 127) {
-    fontstep = 12;
-  }
-  //  Serial.print(x);
-  //  Serial.print(",");
-  //  Serial.println(y);
   u8g2.setCursor(x, y + 12 );
-  u8g2.print(l);
-  //  Serial.print(l);
-  //  Serial.println(int(l));
-  //  Serial.println();
-  x = x + fontstep;
+  if (l < 127) {
+    plot_asc();
+  }
+  else {
+    plot_han();
+  }
+
   if (x > 128 - 12)
   {
     x = 0;
